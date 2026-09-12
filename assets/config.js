382c1ea2f33bf93b5240e86339d3caabdb8fd9db
dn-lx <don.alex.perera@gmail.com>
2026-09-12 16:08:41 +0200

    Add staging mail configuration

---
+++ assets/config.js
@@ -0,0 +1,8 @@
+export const CONFIG = {
+  appName: 'FrankiFlow Mail',
+  mailbox: 'info@frankiflow.de',
+  supabaseUrl: 'https://bdeajozhylypiidrldka.supabase.co',
+  supabasePublishableKey: '',
+  sendFunctionUrl: 'https://bdeajozhylypiidrldka.supabase.co/functions/v1/send-mail',
+  mode: 'demo'
+};
