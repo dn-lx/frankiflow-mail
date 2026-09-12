a8499bdff4d7568832b1b2dd37e68e366c3cdc9e
dn-lx <don.alex.perera@gmail.com>
2026-09-12 16:11:22 +0200

    Connect develop mail app to Supabase

--- assets/config.js
+++ assets/config.js
@@ -2,7 +2,7 @@ export const CONFIG = {
   appName: 'FrankiFlow Mail',
   mailbox: 'info@frankiflow.de',
   supabaseUrl: 'https://bdeajozhylypiidrldka.supabase.co',
-  supabasePublishableKey: '',
+  supabasePublishableKey: 'sb_publishable_FX7QKUBZQmwykGcgv8SdfQ_lGRZF8n2',
   sendFunctionUrl: 'https://bdeajozhylypiidrldka.supabase.co/functions/v1/send-mail',
-  mode: 'demo'
+  mode: 'develop'
 };
